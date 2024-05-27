5aacfce87f77cabcc5dddaf4e223290ad9c2d361
Łukasz Dajtrowski <s29381@pjwstk.edu.pl>
2024-05-27 12:43:32 +0200

    Start

---
+++ WebApplication1/WebApplication1/Program.cs
@@ -0,0 +1,25 @@
+
+using WebApplication1.Repositories;
+using WebApplication1.Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
